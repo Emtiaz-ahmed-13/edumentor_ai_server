070eac52ded123ead47ba05bfce51fac1442faa5
maishashahin <maisha.shahin@g.bracu.ac.bd>
2026-03-27 01:39:17 +0600

    Added voice api  feature

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const AuthRoutes = require("../modules/Auth/auth.route");
 const AIRoutes = require("../modules/AI/ai.route");
 const ConceptRoutes = require("../modules/Concept/concept.route");
-const DocumentQARoutes = require("../modules/DocumentQA/documentQA.route");
 const NoteRoutes = require("../modules/Note/note.route");
 
 const moduleRoutes = [
@@ -18,10 +17,6 @@ const moduleRoutes = [
   {
     path: "/concepts",
     route: ConceptRoutes,
-  },
-  {
-    path: "/document-qa",
-    route: DocumentQARoutes,
   },
   {
     path: "/notes",
