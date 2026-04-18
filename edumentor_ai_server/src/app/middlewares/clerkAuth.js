106953b653376b27d2770d3da67ed005807e7272
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2026-04-18 22:42:29 +0600

    fix

--- edumentor_ai_server/src/app/middlewares/clerkAuth.js
+++
@@ -1,7 +0,0 @@
-const { ClerkExpressRequireAuth } = require('@clerk/clerk-sdk-node');
-
-const clerkAuth = ClerkExpressRequireAuth({
-  // The SDK will automatically use CLERK_SECRET_KEY and CLERK_PUBLISHABLE_KEY from env
-});
-
-module.exports = clerkAuth;
--- edumentor_ai_server/src/app/modules/Concept/concept.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const conceptController = require("./concept.controller");
-const router = express.Router();
-
-router.get("/history", conceptController.getHistory);
-router.get("/:id", conceptController.getConceptById);
-router.patch("/:id", conceptController.updateConcept);
-router.delete("/:id", conceptController.deleteConcept);
-module.exports = router;
